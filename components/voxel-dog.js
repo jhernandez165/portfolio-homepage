a66d1a369fc41bcb4409c4d29c035cb5757972ea
Jonathan Hernandez <jhernandez165@mail.stmarytx.edu>
2023-08-21 17:48:17 -0500

    Added voxel art

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -11,11 +11,20 @@ function easeOutCirc(x) {
 const VoxelDog = () => {
   const refContainer = useRef()
   const [loading, setLoading] = useState(true)
-  const refRenderer = useRef()
-  const urlDogGLB = (process.env.NODE_ENV === 'production' ? 'https://craftzdog.global.ssl.fastly.net/homepage' : '') + '/dog.glb'
+  const [renderer, setRenderer] = useState()
+  const [_camera, setCamera] = useState()
+  const [target] = useState(new THREE.Vector3(0, 0.7, 0))
+  const [initialCameraPosition] = useState(
+    new THREE.Vector3(
+      100 * Math.sin(0.2 * Math.PI),
+     100,
+      100 * Math.cos(0.2 * Math.PI)
+    )
+  )
+  const [scene] = useState(new THREE.Scene())
+  const [_controls, setControls] = useState()
 
   const handleWindowResize = useCallback(() => {
-    const { current: renderer } = refRenderer
     const { current: container } = refContainer
     if (container && renderer) {
       const scW = container.clientWidth
@@ -23,12 +32,12 @@ const VoxelDog = () => {
 
       renderer.setSize(scW, scH)
     }
-  }, [])
+  }, [renderer])
 
   /* eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
     const { current: container } = refContainer
-    if (container) {
+    if (container && !renderer) {
       const scW = container.clientWidth
       const scH = container.clientHeight
 
@@ -40,19 +49,11 @@ const VoxelDog = () => {
       renderer.setSize(scW, scH)
       renderer.outputEncoding = THREE.sRGBEncoding
       container.appendChild(renderer.domElement)
-      refRenderer.current = renderer
-      const scene = new THREE.Scene()
-
-      const target = new THREE.Vector3(-0.5, 1.2, 0)
-      const initialCameraPosition = new THREE.Vector3(
-        20 * Math.sin(0.2 * Math.PI),
-        10,
-        20 * Math.cos(0.2 * Math.PI)
-      )
+      setRenderer(renderer)
 
       // 640 -> 240
       // 8   -> 6
-      const scale = scH * 0.005 + 4.8
+      const scale = scH * 0.00001 + 2
       const camera = new THREE.OrthographicCamera(
         -scale,
         scale,
@@ -63,6 +64,7 @@ const VoxelDog = () => {
       )
       camera.position.copy(initialCameraPosition)
       camera.lookAt(target)
+      setCamera(camera)
 
       const ambientLight = new THREE.AmbientLight(0xcccccc, 1)
       scene.add(ambientLight)
@@ -70,14 +72,28 @@ const VoxelDog = () => {
       const controls = new OrbitControls(camera, renderer.domElement)
       controls.autoRotate = true
       controls.target = target
+      setControls(controls)
 
-      loadGLTFModel(scene, urlDogGLB, {
+      loadGLTFModel(scene, '/prince.gltf', {
         receiveShadow: false,
         castShadow: false
       }).then(() => {
         animate()
         setLoading(false)
       })
+
+      function addStar() {
+        const geometry = new THREE.SphereGeometry(.02, 24, 24);
+        const material = new THREE.MeshStandardMaterial({ color: 0xFFFF00});
+        const star = new THREE.Mesh(geometry, material);
+        const [x, y, z] = Array(3)
+          .fill()
+          .map(() => THREE.MathUtils.randFloatSpread(10));
+        star.position.set(x, y, z);
+        scene.add(star);
+      }
+  
+      Array(50).fill().forEach(addStar);
 
       let req = null
       let frame = 0
@@ -90,8 +106,8 @@ const VoxelDog = () => {
           const p = initialCameraPosition
           const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20
 
-          camera.position.y = 10
-          camera.position.x =
+          camera.position.y = 0
+          camera.position.x = 
             p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed)
           camera.position.z =
             p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed)
@@ -104,8 +120,8 @@ const VoxelDog = () => {
       }
 
       return () => {
+        console.log('unmount')
         cancelAnimationFrame(req)
-        renderer.domElement.remove()
         renderer.dispose()
       }
     }
@@ -116,7 +132,7 @@ const VoxelDog = () => {
     return () => {
       window.removeEventListener('resize', handleWindowResize, false)
     }
-  }, [handleWindowResize])
+  }, [renderer, handleWindowResize])
 
   return (
     <DogContainer ref={refContainer}>{loading && <DogSpinner />}</DogContainer>
