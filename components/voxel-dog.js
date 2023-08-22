f7dcc54e1250bf76071ed428610bc5483e4538e9
Jonathan Hernandez <jhernandez165@mail.stmarytx.edu>
2023-08-21 19:32:35 -0500

    Edit file

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -16,9 +16,9 @@ const VoxelDog = () => {
   const [target] = useState(new THREE.Vector3(0, 0.7, 0))
   const [initialCameraPosition] = useState(
     new THREE.Vector3(
-      1000 * Math.sin(0.2 * Math.PI),
+      1000 ,
      1000,
-      1000 * Math.cos(0.2 * Math.PI)
+      1000
     )
   )
   const [scene] = useState(new THREE.Scene())
